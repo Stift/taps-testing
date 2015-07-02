dabc7911a242c3610460e53315ee52d3c44039c4
Christian Fiebrig <christian.fiebrig@philips.com>
2015-07-02 14:17:17 +0200

    preliminary structural commit

---
+++ TapParserTests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Taps;
+
+namespace TapParserTests
+{
+    [TestFixture]
+    public class TapParserTests
+    {
+
+        [Test]
+        public void Method_Name()
+        {
+            // arrange
+            var sut = new TAPParser();
+            
+            // act
+            sut.ParseLine("1..10");
+
+
+            // assert
+
+        }
+		
+    }
+}
